787452fc5d16b7947b66936ebf5ee36f7bcd6b5d
Artem <sunix@internet.ru>
2021-09-01 22:41:02 +0500

    7-12 tasks

--- 02-components/07-PageMeetup/PageMeetup.js
+++ 02-components/07-PageMeetup/PageMeetup.js
@@ -1,7 +1,10 @@
 import { defineComponent } from './vendor/vue.esm-browser.js';
+
 import UiContainer from './UiContainer.js';
 import UiAlert from './UiAlert.js';
-// import { fetchMeetupById } from './meetupService.js';
+
+import { fetchMeetupById } from './meetupService.js';
+import MeetupView from '../06-MeetupView/MeetupView.js';
 
 export default defineComponent({
   name: 'PageMeetup',
@@ -9,18 +12,62 @@ export default defineComponent({
   components: {
     UiAlert,
     UiContainer,
+    MeetupView,
+  },
+
+  props: {
+    meetupId: {
+      type: Number,
+    },
+  },
+
+  data() {
+    return {
+      meetupItem: null,
+      errorFlag: false,
+      errorMessage: '',
+    };
+  },
+
+  computed: {
+    loader() {
+      return this.meetupItem === null && this.errorFlag === false;
+    },
+  },
+
+  watch: {
+    meetupId() {
+      this.getMeetup();
+    },
+  },
+
+  mounted() {
+    this.getMeetup();
+  },
+
+  methods: {
+    getMeetup() {
+      this.meetupItem = null;
+      this.errorFlag = false;
+      fetchMeetupById(this.meetupId)
+        .then((result) => {
+          this.meetupItem = result;
+        })
+        .catch((error) => {
+          this.errorMessage = error.message;
+          this.errorFlag = true;
+        });
+    },
   },
 
   template: `
     <div class="page-meetup">
-      <!-- meetup view -->
-
-      <ui-container>
+      <meetup-view :meetup='meetupItem' v-if='meetupItem'></meetup-view>
+      <ui-container v-if='loader'>
         <ui-alert>Загрузка...</ui-alert>
       </ui-container>
-
-      <ui-container>
-        <ui-alert>error</ui-alert>
+      <ui-container v-if='errorFlag'>
+        <ui-alert>{{ errorMessage }}</ui-alert>
       </ui-container>
     </div>`,
 });
